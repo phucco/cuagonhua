b48afdf80080dd30e2e10a33a689cc48d0bde268
Admicro <phuchoangminh@admicro.vn>
2017-10-19 18:14:02 +0700

    slide

--- application/views/site/homepage/slide.php
+++ application/views/site/homepage/slide.php
@@ -6,6 +6,9 @@
         <img src="<?php echo base_url(); ?>upload/slide/1600x769-3.jpg" alt="Slide Cửa gỗ nhựa Hoàng Hải 3" />
     </div>
     <div class="first-image">
-        <img src="<?php echo base_url(); ?>upload/slide/1600x769-2.jpg" alt="Slide Cửa gỗ nhựa Hoàng Hải 2" />
+        <img src="<?php echo base_url(); ?>upload/slide/1600x700-3.jpg" alt="Slide Cửa gỗ nhựa Hoàng Hải 2" />
+    </div>
+    <div class="first-image">
+        <img src="<?php echo base_url(); ?>upload/slide/1600x700.jpg" alt="Slide Cửa gỗ nhựa Hoàng Hải 4" />
     </div>
 </div>
